6ac69fa7ea5aabd1780dd4a9f958e9f4f7131dd4
chenwj <731028571@qq.com>
2018-06-28 16:01:45 +0800

    add core file

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://api.github.com'
+
+const giStore = {
+    $http: axios,
+    token: false,
+    gistId: false,
+    setId (id) {
+        this.gistId = id
+    },
+    setToken (token) {
+        axios.defaults.headers.common['Authorization'] = `token ${token}`
+        this.token = true
+    },
+    getList () {
+        if (!this.token) return Promise.reject()
+        return axios({url: '/gists'}).then(res => res.status == 200 ? res.data : Promise.reject(res))
+    },
+    getGist (id) {
+        if (!this.token) return Promise.reject()
+        return axios({url: `/gists/${id}`}).then(res => res.status == 200 ? res.data : Promise.reject(res))
+    },
+    createGist (desc = 'create by gistore', files = {newDb: ''}) {
+        if (!this.token) return Promise.reject()
+        for (let name in files) {
+            files[name] = {content: JSON.stringify(files[name])}
+        }
+        let postConfig = {
+            description: desc,
+            public: true,
+            files: files
+        }
+        return axios({method: 'post', url: '/gists', data: postConfig}).then(res => res.status == 201 ? res.data : Promise.reject(res))
+    },
+    editGist (id, files) {
+        if (!this.token) return Promise.reject()
+        for (let name in files) {
+            files[name] = {content: files[name]}
+        }
+        let postConfig = {
+            public: true,
+            files: files
+        }
+        return axios({method: 'patch', url: '/gists/' + id, data: postConfig}).then(res => res.status == 200 ? res.data : Promise.reject(res))
+    },
+    backUp (files, isJson = true) {
+        if (!this.gistId) return Promise.reject()
+        if (isJson) {
+            for (let name in files) {
+                files[name] = JSON.stringify(files[name])
+            }
+        }
+        return this.editGist(this.gistId, files)
+    },
+    sync () {
+        if (!this.gistId) return Promise.reject()
+        return axios({url: `/gists/${this.gistId}`})
+            .then(res => res.status === 200 ? res.data : Promise.reject(res))
+            .then(res => {
+                let ret = {}
+                for (let name in res.files) {
+                    ret[name] = res.files[name].content
+                    try {
+                        ret[name] = JSON.parse(ret[name])
+                    } catch (e) {}
+                }
+                return ret
+            }, err => Promise.reject(err))
+    },
+    createBackUp (desc, files) {
+        return this.createGist(desc, files)
+            .then(res => {
+                this.setId(res.id)
+                return res.id
+            })
+    }
+}
+
+export default giStore
